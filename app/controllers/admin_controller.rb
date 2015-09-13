02bee1e7a2362c72315f4d1f6e215f2275bed4b4
Ernesto Nobmann <nobmann88@gmail.com>
2015-09-13 15:29:29 -0500

    Session Handling Added
    - When the user authenticates a session variable is instantiated
    - If the user tries to enter the admin controller will be redirected to the home screen
    - admin/logout route added
    - admin.html.erb indent fixed

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,12 @@
 class AdminController < ApplicationController
 	def index
+		if !session[:user_id]
+			redirect_to root_path
+		end
+	end
+	def logout
+		session.delete(:user_id)
+		@current_user = nil
+		redirect_to root_path
 	end
 end
