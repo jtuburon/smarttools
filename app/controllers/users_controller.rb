d260ce814e6a8012cb43e10490450eb74f1162a4
Julio Mario <jtuburon@gmail.com>
2015-09-13 19:14:52 -0500

    user id on sessionf after registration!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
 		else
 			@user = User.new(user_params)
 			if @user.save
+				session[:user_id] = @user.id
 				render :js => "window.location = '/admin/index'"
 			else
 				render 'new'
