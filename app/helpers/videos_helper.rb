49e631cfe58848a53f9290f99bb047c9559e6e69
Julio Mario <jtuburon@gmail.com>
2015-09-18 13:16:46 -0500

    Conversion issues with JW-Player fixed!!!

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,7 +1,6 @@
 module VideosHelper
 	def all_converted_videos_in_competition(competition)
-		#@videos= Video.where(["competition_id = ? and converted_at IS NOT NULL", @competition]).order('created_at DESC').paginate(:page => params[:page], :per_page => 2);
-		@videos= Video.where(competition: competition).order('created_at DESC').paginate(:page => params[:page], :per_page => 2)
+		@videos= Video.where(["competition_id = ? and converted_at IS NOT NULL", @competition]).order('created_at DESC').paginate(:page => params[:page], :per_page => 2);
 	end
 
 	def self.convert_pending_videos
@@ -14,7 +13,7 @@ module VideosHelper
 			c_folder= Rails.root.join("public", "uploads", "#{video.class.to_s.underscore}", "#{video.id}", "c_video")
 			Dir.mkdir(c_folder) unless File.exists?(c_folder)
 			c_filename= Rails.root.join("public", "uploads", "#{video.class.to_s.underscore}", "#{video.id}", "c_video", b_name)
-			movie.transcode(c_filename, "-acodec aac -vcodec libx264 -strict -2")
+			movie.transcode(c_filename, "-acodec aac -vcodec libx264 -profile:v high -strict -2")
 			c_video= "/uploads/#{video.class.to_s.underscore}/#{video.id}/c_video/" +b_name
 			video.c_video = c_video
 			video.converted_at = DateTime.now
