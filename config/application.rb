09879fe14aef32425e794bfe6f1bff75d68f7fe5
Julio Mario <jtuburon@gmail.com>
2015-11-06 16:55:45 -0500

    compile assets removed

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Smarttools
     config.time_zone = 'Bogota'
     #config.active_record.default_timezone = 'Bogota'
 
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
